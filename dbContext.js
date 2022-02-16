a426ec6929ee53ba91ebf797e718936547d324ee
Ali, Imran <imran.ali@tylertech.com>
2022-02-15 23:48:03 -0500

    logging

--- dbContext.js
+++ dbContext.js
@@ -4,6 +4,7 @@ const db = require('diskdb');
 module.exports = {
     connect: function () {
         db.connect(config.db.path, [config.db.name]);
+        console.log("Successfully connected to diskdb.");
         return db;
     }
   };
